6a2471c261ee0007aacc8f4f6c6b2e0fd884abef
Harlabr <typoifrik@protonmail.com>
2026-08-14 11:14:52 +0700

    Update spam.js

--- spam.js
+++ spam.js
@@ -22,7 +22,7 @@
     }
 
     async function fire(token) {
-        const text = "ИЗМЕНИ ЭТО REPO GITHUB github.com/Harlabr/itd-spam"; 
+        const text = "GITHUB github.com/Harlabr/itd-spam"; 
         try {
             const res = await fetch('/api/posts', {
                 method: 'POST',
